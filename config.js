196a67ae9a6cab7a9859f6035369183f0365cd42
Nice3point <nice3point@gmail.com>
2025-12-31 11:27:20 +0300

    Init commit

---
+++ config.js
@@ -0,0 +1,37 @@
+// Renovate (self-hosted) configuration for GitHub platform
+// Reference: https://docs.renovatebot.com/self-hosted-configuration/
+
+module.exports = {
+    // Baseline preset.
+    extends: [
+        "group:monorepos",
+        "group:recommended",
+        "replacements:all",
+        "workarounds:all",
+        ":configMigration",
+        "abandonments:recommended"
+    ],
+
+    // Managed repositories
+    repositories: [
+        "Nice3point/RevitApi",
+        "Nice3point/RevitToolkit",
+        "Nice3point/RevitExtensions",
+        "Nice3point/RevitTemplates",
+        "Nice3point/Revit.Build.Tasks",
+        "Nice3point/RevitUnit",
+        "Nice3point/RevitBenchmark"
+    ],
+
+    // Create an Issue with a dashboard for all updates
+    dependencyDashboard: false,
+    // Disable creation of the onboarding PR (Configure Renovate) in new repositories
+    onboarding: true,
+    prHourlyLimit: 0,
+    prConcurrentLimit: 0,
+
+    labels: ["maintenance ⚙️"],
+
+    // Disable semantic commits (removes chore(deps): prefix)
+    semanticCommits: "disabled"
+};
